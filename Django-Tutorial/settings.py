a12f078aafe45f806550df84f7454419388c8e49
Daniel Castro <dancassig@gmail.com>
2016-04-10 18:01:30 -0500

    Changes to MySQL

--- Django-Tutorial/settings.py
+++ Django-Tutorial/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'Django-Tutorial.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'myproject',
+        'USER': 'daniel',
+        'PASSWORD': '12345',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
